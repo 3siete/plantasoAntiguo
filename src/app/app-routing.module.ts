fee1f4529648e5cd9d44af8d76dd17bd24f83921
“m” <matteoramirezz23@gmail.com>
2023-07-17 17:39:05 -0300

    creacion de rutas para la comunicacion entre componentes dentro de auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   //lazyLoaging
   {
-    path:'',
+    path:'auth',
     loadChildren:()=>import('./modules/auth/auth.module').then(m => m.AuthModule)
   }
 ];
