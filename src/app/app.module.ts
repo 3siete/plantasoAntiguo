dcb5d4789f48301fb5940186c79f5434d207ae12
MateoGs013 <mateogabus@gmail.com>
2023-07-19 10:50:11 -0300

    feat: creacion del componente card-shop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardPostComponent } from './shared/components/card-post/card-post.component';
+import { CardShopComponent } from './share/components/card-shop/card-shop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppRoutingModule,
     CardPostComponent,
+    CardShopComponent,
     
   ],
   imports: [
