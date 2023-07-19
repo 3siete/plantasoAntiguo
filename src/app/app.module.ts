06a173b97b9aa8f5a11b2eb538d23c3d64741c57
MateoGs013 <mateogabus@gmail.com>
2023-07-19 02:45:16 -0300

    feat: creacion del componente card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardPostComponent } from './shared/components/card-post/card-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppRoutingModule,
+    CardPostComponent,
     
   ],
   imports: [
