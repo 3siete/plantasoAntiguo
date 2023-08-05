649208623348e0ac43ba5f44c55f72d5076037ca
mateo <matteoramirezz23@gmail.com>
2023-08-05 16:59:52 -0300

    feat: creacion de componente productos

--- src/app/modulo-prueba/modulo-prueba.module.ts
+++ src/app/modulo-prueba/modulo-prueba.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InicioComponent } from './inicio/inicio.component';
+import { ProductosComponent } from './productos/productos.component';
 
 
 
 @NgModule({
   declarations: [
-    InicioComponent
+    InicioComponent,
+    ProductosComponent
   ],
   imports: [
     CommonModule
